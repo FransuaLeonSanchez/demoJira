9ca0db53c4aff59358e754106bad2bc762d229ba
FransuaLeonSanchez <FransuaLeonSanchez@users.noreply.github.com>
2025-06-14 16:06:22 -0500

    SCRUM-6: Enhance Forums search placeholder - Added more descriptive search placeholder text

--- src/components/Forums.tsx
+++ src/components/Forums.tsx
@@ -289,7 +289,7 @@ function Forums() {
                 <div className="flex-1 relative">
                   <input
                     type="text"
-                    placeholder="Buscar en los foros..."
+                    placeholder="Buscar discusiones, temas o autores..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     className="w-full pl-10 pr-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
